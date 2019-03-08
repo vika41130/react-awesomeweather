58daf8047ac0dd7e97976287881ce8d42dcc789c
VN\dung.phan <dung.phan@vn.defide.co.jp>
2019-03-08 13:13:47 +0700

    #develope - first commit on develope

--- src/App.js
+++ src/App.js
@@ -1,28 +1,83 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Tiles from './components/Tiles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = "4a2d96cceba1c7539f3c973c9d6d49e0";
 
 class App extends Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+
+    if(city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: "",
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the value",
+      });
+    }
+
+
+    console.log(this.state);
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Tiles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form  getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
---
+++ src/components/Tiles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Tiles = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        <h3>Find out temperature, conditions and more...</h3>
+    </div>
+);
+
+export default Tiles;
